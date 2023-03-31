078478ce5dbd64883c53d694354344ffc460bd73
James Gordo <gordo.jt@sprobe.com>
2023-03-31 08:58:42 +0800

    Updated seed handler.

--- src/seed.js
+++ src/seed.js
@@ -5,34 +5,31 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = async (fileName) => {
-const { MONGODB_PORT, MONGODB_HOST, MONGODB_USER, MONGODB_PASSWORD } = process.env;
-const dbUri = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}`;
-await mongoose.connect(dbUri, { useNewUrlParser: true }).then(async (connection) => {
-  const dir = path.join(__dirname, './database/seeders');
-
-  if (fileName) {
-    let file = path.join(dir, `/${fileName.split('.js')[0]}.js`);
-    if (fs.existsSync(file)) {
-      const seeder = require(file);
-      await seeder();
-    }
-  } else {
-    console.log('\n', ' INFO '.bgBlue, 'Seeding Database.', '\n');
-    // run all seeders if fileName not specified
-    fs.readdirSync(dir).forEach(async (file) => {
-      const filePath = path.join(dir, file.split('.js')[0]);
-      const seeder = require(filePath);
-      await seeder();
-      console.log(filePath, ' DONE '.green.bold);
-    });
+  if (!fileName) {
+    console.log('\n', ' ERROR '.bgRed, 'No file specified.', '\n');
+    process.exit(1);
   }
 
-  // close connection
-  await connection.disconnect();
-  process.exit(0);
-}).catch((e) => {
-  const { message } = e;
-  console.log('Database Error:'.red , message);
-});
-}
+  const dir = path.join(process.cwd(), 'src/database/seeders');
+  let file = path.join(dir, `/${fileName.split('.js')[0]}.js`);
+  if (!fs.existsSync(file)) {
+    console.log('\n', ' ERROR '.bgRed, 'File does not exist.', '\n');
+    process.exit(1);
+  }
 
+  const { MONGODB_PORT, MONGODB_HOST, MONGODB_USER, MONGODB_PASSWORD } = process.env;
+  const dbUri = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}`;
+  await mongoose.connect(dbUri, { useNewUrlParser: true }).then(async (connection) => {
+    console.log('\n', ' INFO '.bgBlue, 'Seeding Database.', '\n');
+    const seeder = require(file);
+    await seeder();
+    console.log(file, ' DONE '.green.bold, '\n');
+
+    // close db connection
+    await connection.disconnect();
+    process.exit(0);
+  }).catch((e) => {
+    const { message } = e;
+    console.log('Database Error:'.red , message);
+  });
+}
